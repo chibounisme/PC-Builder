f75a4c03b0e51538cf2bee31644d2ddecfc6d8d2
Mohamed Chiboub <mohamedchiboub97@gmail.com>
2020-12-13 15:15:44 +0100

    updates

--- lib/screens/loginscreen.dart
+++ lib/screens/loginscreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:pcbuilder/screens/homescreen.dart';
-import 'package:pcbuilder/screens/pickgraph.dart';
 
 import 'package:pcbuilder/screens/registerscreen.dart';
 import 'package:pcbuilder/services/auth.dart';
