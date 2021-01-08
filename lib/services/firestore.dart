242d98426c4579be235de6de1ea5bfe0f70b542e
Mohamed Chiboub <mohamedchiboub97@gmail.com>
2021-01-08 16:51:59 +0100

    added search bar for 'pick graph' page

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -6,21 +6,20 @@ class FireStoreService extends ChangeNotifier {
   FireStoreService();
 
   static Future<dynamic> getEquipmentsByType(String equipmentType) async {
-    List<Equipment> equipments = [];
     QuerySnapshot qs = await FirebaseFirestore.instance
         .collection('equipments')
         .where("type", isEqualTo: equipmentType)
         .get();
-    qs.docs.forEach((element) {
-      equipments.add(Equipment(
-        name: element.data()['name'],
-        description: element.data()['description'],
-        type: element.data()['type'],
-        imgUrl: element.data()['img_url'],
-        price: element.data()['price'].toDouble(),
-        brand: element.data()['brand'],
-      ));
-    });
-    return equipments;
+    return qs.docs
+        .map((element) => Equipment(
+              id: element.id,
+              name: element.data()['name'],
+              description: element.data()['description'],
+              type: element.data()['type'],
+              imgUrl: element.data()['img_url'],
+              price: element.data()['price'].toDouble(),
+              brand: element.data()['brand'],
+            ))
+        .toList();
   }
 }
